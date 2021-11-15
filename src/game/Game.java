6030ad71460cff387eae141e4a82d0b4698fc0a0
KevOneRedOne <kevinalves@hotmail.fr>
2021-11-15 14:54:43 +0100

    Debut du projet, creation du damier(methode provisoire)

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,10 +1,49 @@
 package game;
 
 public class Game {
-
+	//Attributs	
+	int sizeTabX = 10;
+	int sizeTabY = 10;
+	
+	char [][] board = new char[sizeTabX][sizeTabY];
+	
+	boolean isGameOn = true;
+	
+	
+	//Constructor
+	public Game() {
+		
+	}
+	
+	
+	
 	public void game() {
 		// TODO Auto-generated method stub
+		fillBoard(board);
 		
+		printBoard(board);
+	}
+	
+	public static void printBoard(char[][] board) {
+		System.out.println();
+		for (int i = 0; i <= board.length-1; i++){
+            for(int j = 0; j <= board.length-1; j++){
+                
+            	System.out.print(" | " + board[j][i]);    
+            }
+            System.out.println(" | ");
+        }
+        System.out.println();
+	}
+	
+	public static void fillBoard(char[][] board) {
+		for(int i=0; i<=board.length-1; i++) {
+			for(int j=0; j<=board.length-1; j++) {
+				board[i][j] = '.';
+				
+							
+			}
+		}
 	}
 
 }

35971ebddc45c16d2a185bdf2455a0cb2c812dbd
KevOneRedOne <kevinalves@hotmail.fr>
2021-11-20 17:24:23 +0100

    Maj global - Menu/Game/ajout de fonction

--- src/utils/Utilitary.java
+++ src/utils/Utilitary.java
@@ -6,13 +6,15 @@ public class Utilitary {
 	
 	public static void printMap(char[][] mapToRead) {
 		System.out.println();
-		System.out.println(" -----------------------------------------");
+		int counter = 1 ;
+		System.out.println(" --1---2---3---4---5---6---7---8---9---10-");
 		for (int i = 0; i < mapToRead.length; i++) {
 			for (int j = 0; j < mapToRead.length; j++) {
 				System.out.print(" | " + mapToRead[j][i]);
 			}
-			System.out.println(" | ");
+			System.out.println(" | "+ counter);
 			System.out.println(" -----------------------------------------");
+			counter++;
 		}
 	}
 	
